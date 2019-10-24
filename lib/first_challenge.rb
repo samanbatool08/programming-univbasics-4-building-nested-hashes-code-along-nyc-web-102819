cbef9add003b0ba5d1240b664a3f319330df62ae
saman batool <samanbatool08@gmail.com>
2019-10-24 16:42:23 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -32,3 +32,12 @@ epic_tragedy = {
     :heroine_friends =? [ ]
   }
 }
+
+epic_tragedy = {
+  :montague => {
+    :patriarch => {name: "Lord Montague", age: "53"},
+    :matriarch => {name: "Lady Montague", age: "54"},
+    :hero => {name: "Romeo", age: "15", status: "alive"},
+    :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}, ]
+  }
+}
